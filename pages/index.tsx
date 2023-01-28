2249dfc2287a50c1a2874b5b293a611c74aedd07
nickarocho <nick@boldlyfine.com>
2023-01-28 10:25:01 -0800

    add react-icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
+import { GiSittingDog } from "react-icons/gi";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -47,7 +47,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <img src="/dog.png" alt="cute puppy icon" className={styles.icon} />
+        <GiSittingDog size={"80px"} />
         <h3>Name my pet</h3>
         <form onSubmit={onSubmit}>
           <input
